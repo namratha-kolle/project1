da5bc967552cfcee36946a5e8896c37a10eb923b
namratha kolle <namratha.g.kolle@gmail.com>
2019-07-02 13:12:58 +0530

    first commit

---
+++ src/bluestone/Scenario1.java
@@ -0,0 +1,9 @@
+package bluestone;
+
+public class Scenario1 {
+	public static void main(String[] args) {
+		System.out.println("hello");
+		System.out.println("hello");
+	}
+
+}
---
+++ src/bluestone/scenario2.java
@@ -0,0 +1,9 @@
+package bluestone;
+
+public class scenario2 {
+	
+	public static void main(String[] args) {
+		System.out.println("hi");
+	}
+
+}
